99123e928dc2072edf6495a5f1044bcd496ffc58
MichiK <48059111+michikxd@users.noreply.github.com>
2020-05-27 03:14:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on("loggedOn", () => {
     console.log(config.username,"You has been loged succesfully.");
 
     client.setPersona(SteamUser.EPersonaState.Online);
-    client.gamesPlayed(["Custom Game Title",730]);
+    client.gamesPlayed(["Custom Game Title",730]);  //730 is id of the game on the steam (730=csgo)
 });
 client.on("friendMessage", function(steamID, message) {
     if (message) {
