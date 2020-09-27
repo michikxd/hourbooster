beebdb218a46027a00a0289b00d51656b31a6fcd
MichiK <48059111+michikxd@users.noreply.github.com>
2020-09-27 21:39:21 +0200

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installing the required modules.
+title Install..
+npm install steam-user
+echo Installation finished!
+pause
---
+++ bot.js
@@ -0,0 +1,20 @@
+const SteamUser = require("steam-user");
+const config = require("./config.json");
+const client = new SteamUser();
+
+const logOnOptions = {
+    accountName: config.username,
+    password: config.password
+};
+
+client.logOn(logOnOptions);
+
+client.on("loggedOn", () => {
+    console.log(config.username,"You has been logged succesfully.");
+
+    client.setPersona(SteamUser.EPersonaState.Online);
+    client.gamesPlayed(["Custom Game Title",730]);  
+    //730 is ID of the Game on the Steam
+    // Ex 730 = CS:GO
+});
+
