7f5cf61ef5e6012adc60d870c2e2e312e605ec24
MichiK <48059111+michikxd@users.noreply.github.com>
2020-11-06 13:54:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on("loggedOn", () => {
 
     client.setPersona(SteamUser.EPersonaState.Online);
     client.gamesPlayed(["Custom Game Title",730,440]);  
-    //730 is ID of the Game on the Steam
+    // 730 is ID of the Game on the Steam
     // Ex 730 = CS:GO
     // You can iddle up to 32 games
 });
